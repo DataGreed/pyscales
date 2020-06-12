b003625ae257fbc3d543c9eb9ef22b4d948799bc
DataGreed <cpr.al.exe@gmail.com>
2020-06-12 21:10:25 +0100

    finished NoteArray

--- pyscales/primitives.py
+++ pyscales/primitives.py
@@ -1,4 +1,4 @@
-
+from copy import copy
 
 
 class Note:
@@ -37,6 +37,10 @@ class Note:
 
         return f"{self.note_name}{self.octave_number}"
 
+    def __repr__(self):
+
+        return f"<Note {self}>"
+
     # TODO: method to return synonym name
 
     @classmethod
@@ -55,16 +59,86 @@ class Note:
         return cls.NOTE_NAME_FLAT_TO_SHARP_SYNONYMS[note_name]
 
 
+class NoteArray:
+    """
+    Structure that returns notes based on supplied indexes.
+    Supports wrapping around and octaves.
+    Zero-based.
+
+    Based on https://stackoverflow.com/questions/22122623/
+    """
+    DEFAULT_NOTE_ORDER = [Note("a"), Note("a#"), Note("b"), Note("c"),
+             Note("c#"), Note("d"), Note("d#"), Note("e"),
+             Note("f"), Note("f#"), Note("g"), Note("g#")]
+
+    def __init__(self, notes=None, simulate_octaves=True):
+        """
+        :param notes: list of ordered notes. If not specified, uses notes in default order
+        starting from C0
+        """
+        self.notes = notes or copy(self.DEFAULT_NOTE_ORDER)
+
+        # this may work unpredictably if notes are not in order or if they are having different octaves
+        # that are not following each other.
+        # note array assumes that notes are more or less in order (left to right - low to high)
+        self.simulate_octaves = simulate_octaves
+
+    def __len__(self):
+        return len(self.notes)
+
+    def __getitem__(self, index):
+
+        if isinstance(index, slice):
+            # Get the start, stop, and step from the slice
+            return [self[ii] for ii in range(*index.indices(10*9))]
+
+        elif isinstance(index, int):
+
+            normalized_index = index % len(self.notes)
+
+            octave_shift = 0
+
+            # NoteArray assumes that notes holds one octave
+            # if the supplied index is out of bounds and is higher than the notes length,
+            # it goes N octaves up. If it's lower - N octaves down.
+            if (index<0 or index >= len(self.notes) ) and self.simulate_octaves:
+                octave_shift = (index- normalized_index) / len(self.notes)
+
+
+            normalized_note = self.notes[normalized_index]
+
+            return Note(note_name=normalized_note.note_name, octave_number=int(normalized_note.octave_number+octave_shift))
+
+    def index(self, note:Note):
+        return self.get_note_index(note)
+
+    def get_note_index(self, note:Note):
+        pass
+
+    def __str__(self):
+
+        return " ".join(self.notes)
+
+
 class ScaleFormula:
     """
     Represents a tonal formula that is used to build up a scale
     from any given root note
     """
+    def __init__(self, formula:list, name:str="Unnamed"):
+
+        if not all(isinstance(i, bool) for i in formula):
+            raise ValueError(f"All value in formula list must be booleans.")
+
+        self.formula = formula
+        self.name = name
+
 
 class Scale:
     """
     Represents a musical scale
     """
+
     pass
 
 
